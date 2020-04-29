96515e5d6a2973c241d93e594be0386272708c18
Henrique Negri <henriquenegri22@gmail.com>
2020-04-29 00:04:16 -0300

    Navegação por rotas nomeadas

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por Categoria'),
+        child: Text('Receitas por Categoria ${category.id}'),
       ),
     );
   }
